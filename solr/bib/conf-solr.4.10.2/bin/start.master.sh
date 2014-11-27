fd148944c97d78aa6bb34649db5ff3e65269686e
Guenter Hipler <guenter.hipler@unibas.ch>
2014-11-27 10:20:23 +0100

    Definitions for latest version 4.10.2

---
+++ solr/bib/conf-solr.4.10.2/bin/start.master.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+LOGFILE=/usr/local/swissbib/solr/tomcat/solrlogs/solr.app.log
+
+function setTimestamp()
+{
+    CURRENT_TIMESTAMP=`date +%Y%m%d%H%M%S`
+}
+
+function startSolr ()
+{
+
+    printf "\n\n" >> $LOGFILE
+    printf "in startSolr ...\n" >> $LOGFILE
+
+    export SOLR_INDEX = ${SOLR_INDEX_BASE}/solrIndexBiblio
+
+    #actually we are using the solr.xml configuration for solr cores
+    #this is depricated since 4.3 / 4.4 -> more investigation necessary
+    #SOLR_INDEX_40=${SOLR_INDEX}/4.3
+
+    echo "SOLR Index: "${SOLR_INDEX}
+
+    ulimit -v unlimited
+
+    JAVA_OPTS="$JAVA_OPTS -Xms8000m -Xmx12000m   -Dswissbib.replication.files=${FILES_TO_REPLICATE}  -Dsolr.data.dir=${SOLR_INDEX}   -Dsolr.solr.home=${SOLR_HOME} -Dsolr.lib.swissbib.dir=${SOLR_CORE} -Dsolr.log.path=$LOGFILE "
+    #JAVA_OPTS="$JAVA_OPTS -Xms4000m -Xmx6000m   -Dsolr.data.dir=${SOLR_INDEX:}   -Dsolr.solr.home=${SOLR_HOME:} -Dsolr.lib.swissbib.dir=${SOLR_CORE:} "
+    export JAVA_OPTS=${JAVA_OPTS}
+    echo "starting Master solr server with JAVA_OPTS:"
+    echo ${JAVA_OPTS}
+
+    printf "Starting Master solr server at   <%s>...\n" ${CURRENT_TIMESTAMP} >> $LOGFILE
+    printf "with JAVA_OPTS   ...\n"  >> $LOGFILE
+
+    printf "${JAVA_OPTS}" >> $LOGFILE
+
+    ${SOLR_TOMCAT}/bin/catalina.sh start -server
+
+
+}
+
+#FILES_TO_REPLICATE="schema.xml,solrconfig.4.3.slave.xml,dr_sys_synonyms.txt,timesynonyms.txt,formsynonyms.txt,stopspellwords.txt"
+
+setTimestamp
+
+startSolr
