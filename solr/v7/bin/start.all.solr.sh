1967dd113eac6dc84a34bebdca2f069a6ee71c98
Guenter Hipler <guenter.hipler@unibas.ch>
2017-11-01 22:50:07 +0100

    add ZK_HOST environment in solr start wrapper

--- solr/v7/bin/start.all.solr.sh
+++ solr/v7/bin/start.all.solr.sh
@@ -3,12 +3,12 @@
 SOLR_BASE=/usr/local/swissbib/solr
 SOLR_RUN=$SOLR_BASE/bin
 SOLR_HOME=/swissbib_index/solrHome7
+export ZK_HOST='sb-uzk1.swissbib.unibas.ch:2181,sb-uzk2.swissbib.unibas.ch:2181,sb-uzk3.swissbib.unibas.ch:2181/solr'
 
 for host in sb-us16.swissbib.unibas.ch sb-us17.swissbib.unibas.ch sb-us18.swissbib.unibas.ch
 do
 
-    ssh swissbib@$host "cd $SOLR_RUN; ./solr start -c -s $SOLR_HOME -p 8080 -z 'sb-uzk1.swissbib.unibas.ch:2181,sb-uzk2.swissbib.unibas.ch:2181,sb-uzk3.swissbib.unibas.ch:2181'"
-
+    ssh swissbib@$host "cd $SOLR_RUN; ./solr start -c -s $SOLR_HOME -p 8080 -z ${ZK_HOST}"
 
 
 done
