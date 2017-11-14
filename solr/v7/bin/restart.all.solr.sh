6f3f5471290618c3921c62187eba0c99ae842188
Guenter Hipler <guenter.hipler@unibas.ch>
2017-11-14 14:02:58 +0100

    changed start / restart scripts for solr nodes in cluster

--- solr/v7/bin/restart.all.solr.sh
+++ solr/v7/bin/restart.all.solr.sh
@@ -3,12 +3,14 @@
 SOLR_BASE=/usr/local/swissbib/solr
 SOLR_RUN=$SOLR_BASE/bin
 SOLR_HOME=/swissbib_index/solrHome7
+
+
 export ZK_HOST='sb-uzk1.swissbib.unibas.ch:2181,sb-uzk2.swissbib.unibas.ch:2181,sb-uzk3.swissbib.unibas.ch:2181/solr'
 
 
 for host in sb-us15.swissbib.unibas.ch sb-us16.swissbib.unibas.ch sb-us17.swissbib.unibas.ch sb-us18.swissbib.unibas.ch
 do
 
-    ssh swissbib@$host "cd ${SOLR_RUN}; ./solr start -c -t ${SOLR_HOME} -p 8080 -z ${ZK_HOST}"
+    ssh swissbib@$host "cd ${SOLR_RUN}; ./solr restart -c -t ${SOLR_HOME} -p 8080 -z ${ZK_HOST}"
 
 done
